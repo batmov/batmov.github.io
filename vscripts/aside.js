c45ce3dd3f8d9d6db4d783c1a58f162bd3b983ce
batmov <67221669+batmov@users.noreply.github.com>
2020-06-24 15:27:42 +0200

    Added folders
    Added fotoview, media, vscripts, apps and downloads folders

---
+++ vscripts/aside.js
@@ -0,0 +1,8 @@
+document.getElementById("vsc-aside").innerHTML = `            
+<a href="http://127.0.0.1:5500/index.html" class="first"><p>Home</p></a>
+<a href="http://127.0.0.1:5500/apps/index.html" class="outter"><p>My apps</p></a>
+    <a href="http://127.0.0.1:5500/apps/fotoview.html" class="inner"><p>FotoView.NET</p></a>
+    <a href="http://127.0.0.1:5500/#" class="inner"><p>CommaLang R&E</p></a>
+<a href="#" class="outter"><p>Repository</p></a>
+    <a href="http://127.0.0.1:5500/#" class="inner"><p>SWF repository</p></a>
+<a href="#" class="outter"><p>Documentation</p></a>`
---
+++ vscripts/header.js
@@ -0,0 +1,8 @@
+document.getElementById("vsc-header").innerHTML = `
+    <nav>
+        <img src="http://127.0.0.1:5500/logo.svg">
+        <a href="http://127.0.0.1:5500/index.html"><p>Home</p></a>
+        <a href="http://127.0.0.1:5500/#"><p>My apps</p></a>
+        <a href="http://127.0.0.1:5500/#"><p>Repository</p></a>
+        <a href="http://127.0.0.1:5500/#"><p>Documentation</p></a>
+    </nav>`
