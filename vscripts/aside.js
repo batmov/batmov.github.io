1ce39ac0471f762244947cd750c3b1f6e824c4ad
batmov <67221669+batmov@users.noreply.github.com>
2020-07-22 23:11:05 +0200

    Update aside.js

--- vscripts/aside.js
+++ vscripts/aside.js
@@ -1,8 +1,8 @@
 document.getElementById("vsc-aside").innerHTML = `            
-<a href="http://127.0.0.1:5500/index.html" class="first"><p>Home</p></a>
-<a href="http://127.0.0.1:5500/apps/index.html" class="outter"><p>Apps</p></a>
-    <a href="http://127.0.0.1:5500/apps/quickvoice.html" class="inner"><p>QuickVoice</p></a>
-<a href="http://127.0.0.1:5500/gethelp" class="outter"><p>Help</p></a>
-<a href="http://127.0.0.1:5500/gethelp/quickvoice.html" class="inner"><p>QuickVoice</p></a>
-<a href="http://127.0.0.1:5500/contact.html" class="outter"><p>Contact</p></a>
+<a href="http://batmov.github.io/index.html" class="first"><p>Home</p></a>
+<a href="http://batmov.github.io/apps/index.html" class="outter"><p>Apps</p></a>
+    <a href="http://batmov.github.io/apps/quickvoice.html" class="inner"><p>QuickVoice</p></a>
+<a href="http://batmov.github.io/gethelp" class="outter"><p>Help</p></a>
+<a href="http://batmov.github.io/gethelp/quickvoice.html" class="inner"><p>QuickVoice</p></a>
+<a href="http://batmov.github.io/contact.html" class="outter"><p>Contact</p></a>
 <a href="#" class="outter"><p>About me</p></a>`
