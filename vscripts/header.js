ee95b13e1ecca2368775fd439f6ee458afc5c908
batmov <67221669+batmov@users.noreply.github.com>
2020-07-22 23:15:38 +0200

    Update header.js

--- vscripts/header.js
+++ vscripts/header.js
@@ -1,9 +1,9 @@
 document.getElementById("vsc-header").innerHTML = `
     <nav>
-        <img src="http://batmov.github.io/logo.svg">
-        <a href="http://batmov.github.io/index.html"><p>Home</p></a>
-        <a href="http://batmov.github.io/apps/index.html"><p>Apps</p></a>
-        <a href="http://batmov.github.io/gethelp/index.html"><p>Help</p></a>
-        <a href="http://batmov.github.io/contact.html"><p>Contact</p></a>
-        <a href="http://batmov.github.io/#"><p>About me</p></a>
+        <img src="https://batmov.github.io/logo.svg">
+        <a href="https://batmov.github.io/index.html"><p>Home</p></a>
+        <a href="https://batmov.github.io/apps/index.html"><p>Apps</p></a>
+        <a href="https://batmov.github.io/gethelp/index.html"><p>Help</p></a>
+        <a href="https://batmov.github.io/contact.html"><p>Contact</p></a>
+        <a href="https://batmov.github.io/#"><p>About me</p></a>
     </nav>`
