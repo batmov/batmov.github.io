9c39c33c6350a5ea39c11b8f797e0e84e86045a3
batmov <67221669+batmov@users.noreply.github.com>
2020-08-27 20:46:46 +0200

    Update header.js

--- scripts/header.js
+++ scripts/header.js
@@ -4,27 +4,27 @@ var JS_MENU = document.getElementById("js-menu")
 
 var JS_MENU_CONTENT = `<div id="js-menu">
 <div class="js-menu-left">
-<a href="http://127.0.0.1:5500/index.html" class="nav"><p>Start</p></a>
-<a href="http://127.0.0.1:5500/index.html" class="nav"><p>Apps</p></a>
-<a href="http://127.0.0.1:5500/index.html" class="nav"><p>Help</p></a>
-<a href="http://127.0.0.1:5500/index.html" class="nav"><p>Contact</p></a>
+<a href="https://batmov.github.io/index.html" class="nav"><p>Start</p></a>
+<a href="https://batmov.github.io/index.html" class="nav"><p>Apps</p></a>
+<a href="https://batmov.github.io/index.html" class="nav"><p>Help</p></a>
+<a href="https://batmov.github.io/index.html" class="nav"><p>Contact</p></a>
 </div>
 <div class="js-menu-rigt"></div>
 </div>`
 
-var HEADER_CONTENT = `<a class="header-img-link" href="http://127.0.0.1:5500/index.html"><img src="http://127.0.0.1:5500/logo.svg"></a>
+var HEADER_CONTENT = `<a class="header-img-link" href="https://batmov.github.io/index.html"><img src="https://batmov.github.io/logo.svg"></a>
 <nav>
-    <a href="http://127.0.0.1:5500/index.html" class="nav"><p>Start</p></a>
+    <a href="https://batmov.github.io/index.html" class="nav"><p>Start</p></a>
     <div class="dropdown">
-        <a href="http://127.0.0.1:5500/apps/index.html" class="nav" id="nav_ir1"><p>Apps</p></a>
+        <a href="https://batmov.github.io/apps/index.html" class="nav" id="nav_ir1"><p>Apps</p></a>
         <div class="dropdown-content">
-            <a href="http://127.0.0.1:5500/apps/quickvoice.html" class="nav"><p>QuickVoice</p></a>
-            <a href="http://127.0.0.1:5500/apps/flashlight.html" class="nav"><p>FlashLight SWF Player</p></a>
+            <a href="https://batmov.github.io/apps/quickvoice.html" class="nav"><p>QuickVoice</p></a>
+            <a href="https://batmov.github.io/apps/flashlight.html" class="nav"><p>FlashLight SWF Player</p></a>
         </div>
     </div>
-    <a href="http://127.0.0.1:5500/gethelp/index.html" class="nav"><p>Help</p></a>
-    <a href="http://127.0.0.1:5500/about.html" class="nav"><p>Contact</p></a>
-    <a href="http://127.0.0.1:5500/about.html" class="nav"><p>About me</p></a>
+    <a href="https://batmov.github.io/gethelp/index.html" class="nav"><p>Help</p></a>
+    <a href="https://batmov.github.io/about.html" class="nav"><p>Contact</p></a>
+    <a href="https://batmov.github.io/about.html" class="nav"><p>About me</p></a>
 </nav>`
 
 
