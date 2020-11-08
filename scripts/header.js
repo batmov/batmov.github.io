9581450c2200f060cff9709086a3bcfbadb5df5c
batmov <67221669+batmov@users.noreply.github.com>
2020-11-08 14:58:26 +0100

    Update

--- scripts/header.js
+++ scripts/header.js
@@ -5,32 +5,42 @@ var JS_MENU = document.getElementById("js-menu")
 var JS_MENU_CONTENT = `<div id="js-menu">
 <div class="js-menu-left">
 <a href="https://batmov.github.io/index.html" class="nav"><p>Start</p></a>
-<a href="https://batmov.github.io/index.html" class="nav"><p>Apps</p></a>
-<a href="https://batmov.github.io/index.html" class="nav"><p>Help</p></a>
-<a href="https://batmov.github.io/index.html" class="nav"><p>Contact</p></a>
+<a href="https://batmov.github.io/tools/index.html" class="nav"><p>Projects</p></a>
+<a href="https://batmov.github.io/contact.html" class="nav"><p>Contact</p></a>
+<a href="https://batmov.github.io/about.html" class="nav"><p>About me</p></a>
 </div>
 <div class="js-menu-rigt"></div>
 </div>`
 
 var HEADER_CONTENT = `<a class="header-img-link" href="https://batmov.github.io/index.html"><img src="https://batmov.github.io/logo.svg"></a>
-<nav>
+<nav id="nav">
     <a href="https://batmov.github.io/index.html" class="nav"><p>Start</p></a>
     <div class="dropdown">
-        <a href="https://batmov.github.io/tools/index.html" class="nav" id="nav_ir1"><p>Projects</p></a>
-        <div class="dropdown-content">
-            <a href="https://batmov.github.io/tools/quickvoice.html" class="nav"><p>QuickVoice</p></a>
+            <a href="https://batmov.github.io/tools/index.html" class="nav" id="nav_ir1"><p>Projects</p></a>
+            <div class="dropdown-content">
             <a href="https://batmov.github.io/tools/flashlight.html" class="nav"><p>FlashLight SWF Player</p></a>
-            <a href="https://batmov.github.io/tools/wand.html" class="nav"><p>Wand for MC: Edu</p></a>
-            <a href="https://batmov.github.io/tools/divideby.html" class="nav"><p>Check if number is dividable</p></a>
+            <a href="https://batmov.github.io/tools/htmlive.html" class="nav"><p>HTMLive</p></a>
+            <a href="https://batmov.github.io/tools/jstools/divideby.html" class="nav"><p>JSTools</p></a>
             <a href="https://batmov.github.io/tools/nativebuttons.html" class="nav"><p>Native buttons</p></a>
+            <a href="https://batmov.github.io/tools/quickvoice.html" class="nav"><p>QuickVoice</p></a>
+            <a href="https://batmov.github.io/tools/wand.html" class="nav"><p>Wand for MC: Edu</p></a>
         </div>
     </div>
     <a href="https://batmov.github.io/contact.html" class="nav"><p>Contact</p></a>
     <a href="https://batmov.github.io/about.html" class="nav"><p>About me</p></a>
-</nav>`
+</nav>
+<div id="jsmenutoggle">
+<a onclick="_$jsmenutoggle()" href="#" class="nav"><img src="https://batmov.github.io/menu.png"></a>
+</div>`
 
 
-HEADER.innerHTML = HEADER_CONTENT
+if (getCookie("darktheme") == "true")
+{
+    HEADER_CONTENT += '<link rel="stylesheet" type="text/css" href="$stylesrc_dark">'.replace("$stylesrc_dark", document.getElementsByTagName('link')[0].href.replace("style.css", "style_dark.css"))
+}
+
+
+HEADER.innerHTML = HEADER_CONTENT + JS_MENU_CONTENT
 
 function _$(url)
 {
@@ -43,8 +53,56 @@ function _$jsmenutoggle()
     {
         JS_MENU_STATUS = false
         document.getElementById("js-menu").style.height = "0px"
-    } else {
+    }
+    else
+    {
         JS_MENU_STATUS = true
         document.getElementById("js-menu").style.height = "250px"
     }
+}
+
+function resize(){
+    if (window.innerWidth < 590)
+    {
+        document.getElementById("nav").style.display = "none"
+        document.getElementById("jsmenutoggle").style.display = "block"
+    }
+    else
+    {
+        document.getElementById("nav").style.display = "block"
+        document.getElementById("jsmenutoggle").style.display = "none"
+    }
+}
+
+window.onresize = function(){
+    if (window.innerWidth < 590)
+    {
+        document.getElementById("nav").style.display = "none"
+        document.getElementById("jsmenutoggle").style.display = "block"
+    }
+    else
+    {
+        document.getElementById("nav").style.display = "block"
+        document.getElementById("jsmenutoggle").style.display = "none"
+    }}
+
+resize()
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function swapStyleSheet(sheet){
+	document.getElementsByTagName('link')[0].setAttribute("href", document.getElementsByTagName('link')[0].href.replace("style.css", sheet));
 }
---
+++ tools/jstools/aside.js
@@ -0,0 +1,12 @@
+var ASIDE = document.getElementById("aside")
+
+var ASIDE_CONTENT = `
+<h3><b>Math</b></h3>
+
+<h3><a href="divideby.html">Number dividable by...</a></h3>
+<h3><b>Text</b></h3>
+<h3><a href="textstats.html">TextStats</a></h3>
+<h3><a href="alphabetencoder.html">Alphabet encoder</a></h3>
+`
+
+ASIDE.innerHTML = ASIDE_CONTENT
